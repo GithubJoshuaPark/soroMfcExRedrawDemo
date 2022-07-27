0cea1d4533544d9bc37fe17c719cb4072c3358a6
JoshuaPark <soromiso@gmail.com>
2022-07-28 08:10:01 +0900

    add bmp file and used CImage

--- soroMfcExRedrawDemoView.cpp
+++ soroMfcExRedrawDemoView.cpp
@@ -4,6 +4,7 @@
 
 #include "pch.h"
 #include "framework.h"
+#include <atlimage.h>
 // SHARED_HANDLERS can be defined in an ATL project implementing preview, thumbnail
 // and search filter handlers and allows sharing of document code with that project.
 #ifndef SHARED_HANDLERS
@@ -31,6 +32,10 @@ BEGIN_MESSAGE_MAP(CsoroMfcExRedrawDemoView, CView)
 	ON_WM_PAINT()
 	ON_WM_LBUTTONUP()
 	ON_WM_MOUSEMOVE()
+	ON_COMMAND(BTN_ID_1, GetImageOfDeskTop)
+	ON_COMMAND(BTN_ID_2, SaveImageOfDeskTop)
+	ON_WM_CREATE()
+	ON_WM_DESTROY()
 END_MESSAGE_MAP()
 
 // CsoroMfcExRedrawDemoView construction/destruction
@@ -38,7 +43,7 @@ END_MESSAGE_MAP()
 CsoroMfcExRedrawDemoView::CsoroMfcExRedrawDemoView() noexcept
 {
 	// TODO: add construction code here
-	m_BtnRect = CRect(500, 100, 700, 300);
+	m_BtnRect = CRect(800, 100, 1000, 300);
 	m_bClicked = FALSE;
 
 	m_RectForText = CRect(0, 0, 0, 0);
@@ -144,29 +149,76 @@ void CsoroMfcExRedrawDemoView::OnPaint()
 	CRect cRect;
 	GetClientRect(&cRect);
 
-	// CDC, CBitmap
+	CFont font, fontForImageDC;	
+	CFont *pOldFont, *pOldFontForImageDC;
+
+	LOGFONT lf;
+	::ZeroMemory(&lf, sizeof(lf));
+	lf.lfHeight = 40;
+	lf.lfUnderline = 1;
+	lf.lfItalic = 1;
+	lf.lfWeight = FW_BOLD;
+	wsprintf(lf.lfFaceName, TEXT("%s"), TEXT("Tahoma"));
+
+	//font.CreatePointFont(400, _T("Tahoma"));   // 타호마 체로 폰트 설정
+	font.CreateFontIndirect(&lf);
+
+	lf.lfHeight = 80;
+	fontForImageDC.CreateFontIndirect(&lf);
+
+	pOldFont = dc.SelectObject(&font);
+
+	LOGBRUSH lb;
+	lb.lbStyle = BS_SOLID;
+	lb.lbColor = RGB(192, 192, 192);  // lightGray
+	DWORD style[] = { 6, 3 };
+
+	CPen pen, * pOldPen, *pOldPenOfImageDC;
+	CBrush brush, * pOldBrush;
+
+	pen.CreatePen(PS_GEOMETRIC | PS_SOLID | PS_ENDCAP_FLAT | PS_INSIDEFRAME | PS_JOIN_ROUND, 1, &lb);
+	brush.CreateSolidBrush(::GetSysColor(COLOR_BTNFACE));
+
+	pOldPen = dc.SelectObject(&pen);
+	pOldBrush = (CBrush*)dc.SelectStockObject(NULL_BRUSH); //Polygan 내부에 채우기를 하지 않도록 블러쉬 설정
+
+
+	// MARK: -  CDC, CBitmap for setting bg bmp image 시작
 	CDC MemDC;
 	BITMAP bmpInfo;
 
 	MemDC.CreateCompatibleDC(&dc);
-	
-	CBitmap bmp, *pOldBmp = NULL;
+
+	CBitmap bmp, * pOldBmp = NULL;
 	bmp.LoadBitmapW(IDB_BITMAP2);
 	bmp.GetBitmap(&bmpInfo); // get info of the bmp
-	
+
 	pOldBmp = MemDC.SelectObject(&bmp);
 
 	dc.BitBlt(0, 0, bmpInfo.bmWidth, bmpInfo.bmHeight, &MemDC, 0, 0, SRCCOPY);
 	MemDC.SelectObject(pOldBmp);
+	// MARK: -  CDC, CBitmap for setting bg bmp image 끝
 
-	CFont font, *pOldFont;
-	//font.CreatePointFont(400, _T("Tahoma"));   // 타호마 체로 폰트 설정
+	// MARK: - Use CImage Start
+	CImage cImage;
+	cImage.LoadFromResource(AfxGetInstanceHandle(), IDB_PORORO);
 
+		CDC* pDC = CDC::FromHandle(cImage.GetDC());  // Get DC of the Image
+			pOldPenOfImageDC = pDC->SelectObject(&pen);
+			pOldFontForImageDC = pDC->SelectObject(&fontForImageDC);
+				pDC->SetBkMode(TRANSPARENT);
+				pDC->SetTextColor(RGB(255,255,0));
+				pDC->TextOutW(10,30, _T("Pororo Bmp"));
+			pDC->SelectObject(pOldPenOfImageDC);
+			pDC->SelectObject(pOldFontForImageDC);
+		cImage.ReleaseDC();
 
-	LOGBRUSH lb;
-	lb.lbStyle = BS_SOLID;
-	lb.lbColor = RGB(255, 0, 0);
-	DWORD style[] = {6, 3};
+	cImage.TransparentBlt(dc.m_hDC,
+		200, 200, 
+		cImage.GetWidth(), cImage.GetHeight(),
+		RGB(255,255,0));
+	// MARK: - Use CImage End
+
 
 	//CPen pen, *pOldPen;
 	//pen.CreatePen(PS_USERSTYLE | PS_GEOMETRIC | PS_ENDCAP_FLAT , 10, &lb, 2, style);
@@ -209,15 +261,6 @@ void CsoroMfcExRedrawDemoView::OnPaint()
 	//dc.TextOutW(cRect.Width()/2, cRect.Height()/2, _T("Good Neighborhood"));
 
 	// 삼각형 그리기
-	CPen pen, * pOldPen;
-	CBrush brush, * pOldBrush;
-
-	pen.CreatePen(PS_GEOMETRIC | PS_SOLID | PS_ENDCAP_FLAT| PS_INSIDEFRAME | PS_JOIN_ROUND, 20, &lb);
-	brush.CreateSolidBrush(::GetSysColor(COLOR_BTNFACE));
-
-	pOldPen = dc.SelectObject(&pen);
-	pOldBrush = (CBrush*) dc.SelectStockObject(NULL_BRUSH); //Polygan 내부에 채우기를 하지 않도록 블러쉬 설정
-
 	// Draw a triangle(ploygan)
 	//dc.BeginPath();
 	//	dc.MoveTo(190, 40);
@@ -227,37 +270,35 @@ void CsoroMfcExRedrawDemoView::OnPaint()
 	//dc.EndPath();
 	//dc.StrokeAndFillPath();
 
-	POINT arPt[3] = { {190, 40},{290, 190},{90, 190} };
-	
-	dc.Polygon(arPt, 3);
+	//POINT arPt[3] = { {190, 40},{290, 190},{90, 190} };
+	//
+	//dc.Polygon(arPt, 3);
+	//dc.Ellipse(100, 100, 160, 160);
 
-	dc.Ellipse(100, 100, 160, 160);
+	//dc.SelectObject(pOldPen);
+	//	dc.Rectangle(100, 100, 160, 160);
+	//dc.SelectObject(pOldBrush);
 
-	dc.SelectObject(pOldPen);
-	dc.Rectangle(100, 100, 160, 160);
-
-	dc.SelectObject(pOldBrush);
-
-	// 기본펜으로 삼각형 그리고
-	dc.MoveTo(190, 40);
-	dc.LineTo(290, 190);
-	dc.LineTo(90, 190);
-	dc.LineTo(190, 40);
+	//// 기본펜으로 삼각형 그리고
+	//dc.MoveTo(190, 40);
+	//dc.LineTo(290, 190);
+	//dc.LineTo(90, 190);
+	//dc.LineTo(190, 40);
 
 
-	// 내각이 270인 부채꼴
-	CRect pieRect_270(400, 200, 800, 600);
-	dc.Pie(&pieRect_270,
-		CPoint(pieRect_270.CenterPoint().x, pieRect_270.top),
-		CPoint(pieRect_270.right, pieRect_270.CenterPoint().y));
+	//// 내각이 270인 부채꼴
+	//CRect pieRect_270(400, 200, 800, 600);
+	//dc.Pie(&pieRect_270,
+	//	CPoint(pieRect_270.CenterPoint().x, pieRect_270.top),
+	//	CPoint(pieRect_270.right, pieRect_270.CenterPoint().y));
 
-	CRect pieRect_90(800, 200, 1200, 600);
-	dc.Pie(&pieRect_90,
-		CPoint(pieRect_90.right, pieRect_90.CenterPoint().y),
-		CPoint(pieRect_90.CenterPoint().x, pieRect_90.top));
+	//CRect pieRect_90(800, 200, 1200, 600);
+	//dc.Pie(&pieRect_90,
+	//	CPoint(pieRect_90.right, pieRect_90.CenterPoint().y),
+	//	CPoint(pieRect_90.CenterPoint().x, pieRect_90.top));
 	
 
-	// Btn 처럼 보이게 그리기
+	// Btn 처럼 보이게 그리기 시작
 	CRect Rect(m_BtnRect);
 	Rect += CRect(4, 4, 4, 4);
 	//dc.Rectangle(&Rect);
@@ -284,16 +325,7 @@ void CsoroMfcExRedrawDemoView::OnPaint()
 	dc.SetBkColor(::GetSysColor(COLOR_BTNFACE));
 	dc.SetTextColor(::GetSysColor(COLOR_BTNTEXT));
 
-	LOGFONT lf;
-	::ZeroMemory(&lf, sizeof(lf));
-	lf.lfHeight = 40;
-	lf.lfUnderline = 1;
-	lf.lfItalic = 1;
-	lf.lfWeight = 1;
-	wsprintf(lf.lfFaceName, TEXT("%s"), TEXT("Arial"));
 
-	font.CreateFontIndirect(&lf);
-	pOldFont = dc.SelectObject(&font);
 
 	if (m_bClicked) {
 		CRect l_cRect = m_BtnRect;
@@ -306,13 +338,13 @@ void CsoroMfcExRedrawDemoView::OnPaint()
 		dc.DrawText(L"Test Button", &m_BtnRect,
 			DT_CENTER | DT_SINGLELINE | DT_VCENTER);
 	}
+	// Btn 처럼 보이게 그리기 끝
 
 	if (m_isDragStart) {
 		dc.Rectangle(&m_RectForText);
 	}
 
-	dc.DrawText(L"Good Neighborhood", &m_RectForText,
-	
+	dc.DrawText(L"Good Neighborhood", &m_RectForText,	
 		DT_CENTER | DT_SINGLELINE | DT_VCENTER);
 
 	int nTabStop = 40;
@@ -374,4 +406,107 @@ void CsoroMfcExRedrawDemoView::OnMouseMove(UINT nFlags, CPoint point)
 	}
 
 	CView::OnMouseMove(nFlags, point);
+}
+
+
+int CsoroMfcExRedrawDemoView::OnCreate(LPCREATESTRUCT lpCreateStruct)
+{
+	if (CView::OnCreate(lpCreateStruct) == -1)
+		return -1;
+
+	// TODO:  Add your specialized creation code here
+	CString captionForCapture = _T("DeskTop Image Capture");
+	CString captionForSave    = _T("DeskTop Image Save");
+	m_childWndForCapture.Create(captionForCapture,
+					WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+					CRect(10, 10, 300, 50), this, BTN_ID_1);
+
+	m_childWndForSave.Create(captionForSave,
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+		CRect(10, 70, 300, 110), this, BTN_ID_2);
+
+	return 0;
+}
+
+
+void CsoroMfcExRedrawDemoView::OnDestroy()
+{
+	CView::OnDestroy();
+
+	// TODO: Add your message handler code here
+}
+
+
+void CsoroMfcExRedrawDemoView::GetImageOfDeskTop()
+{
+	// TODO: Add your implementation code here.
+	OutputDebugString(_T("\n※★: CsoroMfcExRedrawDemoView::GetImageOfDeskTop Start\n"));
+
+	CWnd* pWndDeskTop = GetDesktopWindow(); 
+	CWindowDC deskTopScrDC(pWndDeskTop);  // 바탕화면 윈도우 DC
+	CClientDC dc(this);
+
+	// 바탕화면 크기, 색상수와 동일한 비트맵 이미지 만들고
+	CImage cImage;
+	cImage.Create(300, 300, deskTopScrDC.GetDeviceCaps(BITSPIXEL));
+
+	// 이미지DC와 화면DC에 바탕화면 윈도우DC를 출력
+	CDC* imageDC = CDC::FromHandle(cImage.GetDC());
+		imageDC->BitBlt(0, 0, 300, 300, &deskTopScrDC, 0, 0, SRCCOPY);
+	cImage.ReleaseDC();
+
+	// 일부(200*200) 영역을 Gray 이미지로 변환
+	COLORREF rgb;
+	for (int x = 0; x < 200; x++) {
+		for (int y = 0; y < 200; y++) {
+			rgb = cImage.GetPixel(x, y);
+			RGBtoGray(rgb);
+			cImage.SetPixel(x, y, rgb);
+		}
+	}
+
+	// (200*200) 영역을 흑백으로 변환시킨 이미지를 화면에 출력
+	CRect cRect;
+	GetClientRect(&cRect);
+
+	cImage.BitBlt(dc.m_hDC, 310, 10);
+
+	m_cImage = &cImage;
+
+	// JPEG 형식으로 바탕화면 이미지를 저장
+	m_cImage->Save(_T("Desktop.jpg"),
+		Gdiplus::ImageFormatJPEG);
+
+	// 저장된 이미지를 뷰어를 (윈도우 탐색기)실행하여 보여준다.
+	//::ShellExecute(NULL,
+	//	TEXT("Open"),
+	//	TEXT("Desktop.jpg"),
+	//	NULL,
+	//	NULL,
+	//	SW_SHOW);
+
+	OutputDebugString(_T("\n※★: CsoroMfcExRedrawDemoView::GetImageOfDeskTop END\n"));
+}
+
+
+void CsoroMfcExRedrawDemoView::SaveImageOfDeskTop()
+{
+	// TODO: Add your implementation code here.
+	OutputDebugString(_T("\n※★: CsoroMfcExRedrawDemoView::SaveImageOfDeskTop Start\n"));
+
+	if (m_cImage != NULL) {
+		// JPEG 형식으로 바탕화면 이미지를 저장
+		//m_cImage->Save(_T("Desktop.jpg"),
+		//	Gdiplus::ImageFormatJPEG);
+
+		// 저장된 이미지를 뷰어를 (윈도우 탐색기)실행하여 보여준다.
+		::ShellExecute(NULL,
+			TEXT("Open"),
+			TEXT("Desktop.jpg"),
+			NULL,
+			NULL,
+			SW_SHOW);
+	}
+	
+	OutputDebugString(_T("\n※★: CsoroMfcExRedrawDemoView::SaveImageOfDeskTop END\n"));
 }
--- soroMfcExRedrawDemoView.h
+++ soroMfcExRedrawDemoView.h
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#define BTN_ID_1 10001
+#define BTN_ID_2 10002
 
 class CsoroMfcExRedrawDemoView : public CView
 {
@@ -24,6 +26,10 @@ public:
 	CPoint m_startPoint;
 	CPoint m_endPoint;
 	BOOL m_isDragStart;
+
+	CButton m_childWndForCapture;
+	CButton m_childWndForSave;
+	CImage* m_cImage;
 
 // Overrides
 public:
@@ -52,6 +58,22 @@ public:
 	afx_msg void OnPaint();
 	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
 	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+
+	// RGB color -> 흑백으로 바꾸기
+	void RGBtoGray(COLORREF& rgb)
+	{
+		// TODO: Add your implementation code here.
+		BYTE byGray = (GetRValue(rgb) * 30
+			+ GetGValue(rgb) * 59
+			+ GetBValue(rgb) * 11) / 100;
+		rgb = RGB(byGray, byGray, byGray);
+	}
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg void OnDestroy();
+private:
+	void GetImageOfDeskTop();
+public:
+	void SaveImageOfDeskTop();
 };
 
 #ifndef _DEBUG  // debug version in soroMfcExRedrawDemoView.cpp
